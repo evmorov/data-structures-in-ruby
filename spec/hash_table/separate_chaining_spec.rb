625d101792cd68d293598e8f8c9fe8b280539e64
Evgeny Morozov <evmorov@gmail.com>
2017-03-04 19:53:34 +0300

    Basic get-set

--- spec/hash_table/separate_chaining_spec.rb
+++ spec/hash_table/separate_chaining_spec.rb
@@ -2,9 +2,15 @@ require 'rspec'
 require_relative '../../src/hash_table/separate_chaining'
 
 describe SeparateChaining do
-  describe '#[]' do
-    it do
-      expect(subject['a']).to eq('a')
+  describe '#[] and #[]=' do
+    it 'works if key is String' do
+      subject['key'] = 'value'
+      expect(subject['key']).to eq('value')
+    end
+
+    it 'works if key is Symbol' do
+      subject[:key] = 'value'
+      expect(subject[:key]).to eq('value')
     end
   end
 end
--- src/hash_table/separate_chaining.rb
+++ src/hash_table/separate_chaining.rb
@@ -1,5 +1,16 @@
 class SeparateChaining
+  def []=(key, value)
+    @st ||= []
+    @st[hash(key)] = value
+  end
+
   def [](key)
-    key
+    @st[hash(key)]
+  end
+
+  private
+
+  def hash(key)
+    key.hash % 100
   end
 end
